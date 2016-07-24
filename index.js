65fbd66f6900145669ed4db206fbc1995c5a0d8d
Nick Roberts <nroberts@andrew.cmu.edu>
2016-07-24 11:03:51 -0400

    Gives me another chance to commit! O ya!

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 /* WHO NEEDS JQUERY NOT ME THAT'S FOR SURE */
 window.onload = function () {
   var showMailElement = document.getElementById("show-mail");
-  showMailElement.href = "http://www.google.com/recaptcha/mailhide/d?k=01qkz0Fr_I3f4cZ0kd1x_7gw==&amp;c=fPANp7eN-f9ECrEMMHqKkGkSd9MbKvYG_mvaYu6rS7s=";
   showMailElement.onclick = function() {
     window.open('http://www.google.com/recaptcha/mailhide/d?k\x3d01qkz0Fr_I3f4cZ0kd1x_7gw\x3d\x3d\x26c\x3dfPANp7eN-f9ECrEMMHqKkGkSd9MbKvYG_mvaYu6rS7s\x3d', '', 'toolbar=0,scrollbars=0,location=0,statusbar=0,menubar=0,resizable=0,width=500,height=300');
     return false;
